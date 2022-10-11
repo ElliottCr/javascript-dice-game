100317c9ad509fcdcc24e2eebc8960e0cc5139ed
ElliottCr <elliottcrampton@protonmail.com>
2022-10-11 18:48:04 +0200

    Add finish conditions.

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,6 +17,10 @@ function reset() {
     rounds = [0, 0]
     currentPlayer = 2;
 
+    // Set player titles back to their default.
+    document.querySelector("#player1 .player-title").innerHTML = "Player 1";
+    document.querySelector("#player2 .player-title").innerHTML = "Player 2";
+
     // Set global scores to 0.
     document.querySelector("#player1 .player-score").innerHTML = "0";
     document.querySelector("#player2 .player-score").innerHTML = "0";
@@ -26,6 +30,10 @@ function reset() {
     document.querySelector("#player1 .hold").addEventListener("click", hold)
     document.querySelector("#player2 .roll").addEventListener("click", roll)
     document.querySelector("#player2 .hold").addEventListener("click", hold)
+
+    // Resets the color back if it was previously set to red.
+    document.querySelector(`#player1 .round`).style.color = "unset";
+    document.querySelector(`#player2 .round`).style.color = "unset";
 
     turn();
 }
@@ -51,27 +59,48 @@ function turn() {
 function roll() {
     let diceRoll = Math.floor(Math.random() * 6) + 1;
     let diceEl = document.querySelector(`#player${currentPlayer} .dice`);
+    let roundEl = document.querySelector(`#player${currentPlayer} .round`)
+
+    // Resets the color back if it was previously set to red.
+    roundEl.style.color = "unset";
 
     // Animate and styles the dice image.
     diceEl.style.backgroundImage = `url('images/dice${diceRoll}.png')`;
     diceEl.classList.remove("dice-anim");
     diceEl.offsetWidth;
     diceEl.classList.add("dice-anim");
-    
+
+    // Play a random dice sound effect.
     diceSfx[Math.floor(Math.random() * 3)].play();
 
+    // When a dice roll is 1, the round doesn't get added to the global.
+
     if (diceRoll == 1) {
+        roundEl.style.color = "red";
         turn();
     } else {
         rounds[currentPlayer - 1] += diceRoll;
-        document.querySelector(`#player${currentPlayer} .round`).innerHTML = `+${rounds[currentPlayer - 1]}`;
+        roundEl.innerHTML = `+${rounds[currentPlayer - 1]}`;
     }
 }
 
 // Adds round value to global value and goes to the next turn.
 function hold() {
-    diceSfx[2+currentPlayer].play();
+    diceSfx[2 + currentPlayer].play();
     globals[currentPlayer - 1] += rounds[currentPlayer - 1];
     document.querySelector(`#player${currentPlayer} .player-score`).innerHTML = globals[currentPlayer - 1];
-    turn();
+
+    if (globals[currentPlayer - 1] >= 100) {
+        finish();
+    } else { turn(); }
+}
+
+function finish() {
+    document.querySelector(`#player${currentPlayer} .player-title`).innerHTML = `Player ${currentPlayer} Wins!`
+
+    // Disable all roll and hold buttons.
+    document.querySelector(`#player1 .hold`).disabled = true;
+    document.querySelector(`#player1 .roll`).disabled = true;
+    document.querySelector(`#player2 .hold`).disabled = true;
+    document.querySelector(`#player2 .roll`).disabled = true;
 }
